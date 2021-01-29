b72e1cfc592148b05755a2a4d310fffbddd0d4b3
amadou-20 <ahmo300@outlook.com>
2021-01-30 00:54:00 +0100

    I add a select form to change the color of the background

---
+++ index.js
@@ -0,0 +1,8 @@
+var box = document.querySelector('.my_div');
+var slt = document.querySelector('.slt');
+
+
+function changecolor() {
+    box.style.backgroundColor = slt.value;
+    box.style.color = "white";
+}
